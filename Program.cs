31942318ead8570abc6d4a876c10ae3fdead4200
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-18 16:25:39 +0530

    [Rupali]feat:Add element in linked list

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to linkedlist implementation!");
+            LinkedList list = new LinkedList();
+            //add
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
         }
     }
 }
