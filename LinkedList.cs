202676a62710cbdb6184fcf1838e8d929e0dbead
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-18 16:58:23 +0530

    [Rupali]feat:Append element

--- LinkedList.cs
+++ LinkedList.cs
@@ -41,6 +41,22 @@ namespace LinkedList
             }
         }
 
+        internal void Append(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                    temp = temp.next;
+                temp.next = node;
+                Console.WriteLine(" Element {0} Appended to the {1} ", node.data, temp.data);
+            }
+        }
         public void Display()
         {
             Node temp = this.head;
